913715daf6caaa91a8b111cdb73004ec4b62eff8
Lloyd Lobo <76430758+lloydlobo@users.noreply.github.com>
2022-12-16 16:51:39 +0530

    docs(easy): add comments

--- easy/0231_power-of-two/rust/src/helpers.rs
+++ easy/0231_power-of-two/rust/src/helpers.rs
@@ -1,5 +1,7 @@
+//! Module helpers for creating helper functions.
+
 pub struct Pow {
-    pub base_pow: fn(i32, u32) -> i32,
+    pub base: fn(i32, u32) -> i32,
 }
 
 impl Pow {
@@ -15,5 +17,48 @@ impl Pow {
     /// ```
     pub fn base(base: i32, exp: u32) -> i32 {
         base.pow(exp)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    /// Computes the power of a base number.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let base = 2;
+    /// let exp = 10;
+    /// let pow = Pow::base(base, exp);
+    /// assert_eq!(1024, pow);
+    /// ```
+    fn test_base_pow() {
+        let exp = 10u32;
+        let base = 2i32;
+
+        let pow = Pow::base(base, exp);
+        assert_eq!(1024, pow);
+    }
+
+    use crate::helpers;
+
+    #[test]
+    /// This function tests the base_pow_exp function
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use helpers::Pow;
+    /// let exp = 10u32;
+    /// let base = 2i32;
+    /// assert_eq!(1024, Pow::base(base, exp));
+    /// ```
+    fn test_base_pow_exp() {
+        let exp = 10u32;
+        let base = 2i32;
+        assert_eq!(1024, helpers::Pow::base(base, exp));
     }
 }
